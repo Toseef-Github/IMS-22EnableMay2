f09ec6dd4df31f402d1d0d1dee36a10dc7bfaeb5
Toseef-Github <105277464+Toseef-Github@users.noreply.github.com>
2022-06-07 18:29:45 +0100

    Created all 3 item classes

--- src/main/java/com/qa/ims/persistence/domain/Item.java
+++ src/main/java/com/qa/ims/persistence/domain/Item.java
@@ -1,10 +1,71 @@
 package com.qa.ims.persistence.domain;
 
+import java.util.Objects;
+
 public class Item {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private long itemId;
+	private String itemName;
+	private double price;
+	
+	public Item(String itemName, double price) {
+		this.setItemName(itemName);
+		this.setPrice(price);
 	}
+
+	public Item(Long itemId, String itemName, double price) {
+		this.setItemId(itemId);
+		this.setItemName(itemName);
+		this.setPrice(price);
+	}
+
+	public long getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(long itemId) {
+		this.itemId = itemId;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "itemId= " + itemId + ", itemName= " + itemName + ", price= " + price;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(itemId, itemName, price);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		return itemId == other.itemId && Objects.equals(itemName, other.itemName)
+				&& Double.doubleToLongBits(price) == Double.doubleToLongBits(other.price);
+	}
+
+	
 
 }
