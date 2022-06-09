d25da156f70ec8808fd5e2930146d24b345a2c72
Toseef-Github <105277464+Toseef-Github@users.noreply.github.com>
2022-06-09 22:59:58 +0100

    created orderitemsDAOtest

---
+++ src/test/java/com/qa/ims/persistence/dao/OrderItemsDAOTest.java
@@ -0,0 +1,60 @@
+package com.qa.ims.persistence.dao;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.qa.ims.persistence.domain.OrderItems;
+import com.qa.ims.utils.DBUtils;
+
+public class OrderItemsDAOTest {
+	
+	private final OrderItemsDAO DAO = new OrderItemsDAO();
+
+	@Before
+	public void setup() {
+		DBUtils.connect();
+		DBUtils.getInstance().init("src/test/resources/sql-schema.sql", "src/test/resources/sql-data.sql");
+	}
+
+	@Test
+	public void testCreate() {
+		final OrderItems created = new OrderItems(1L, 1L, 1L, 1L);
+		assertEquals(created, DAO.create(created));
+	}
+
+	@Test
+	public void testReadAll() {
+		List<OrderItems> expected = new ArrayList<>();
+		expected.add(new OrderItems(1L, 1L, 1L, 1L));
+		assertEquals(expected, DAO.readAll());
+	}
+
+	@Test
+	public void testReadLatest() {
+		assertEquals(new OrderItems(1L, 2L, 1L, 1L), DAO.readLatest());
+	}
+
+	@Test
+	public void testRead() {
+		final long order_Items_id = 1L;
+		assertEquals(new OrderItems(order_Items_id, 2L, 1L, 1L), DAO.read(order_Items_id));
+	}
+
+	@Test
+	public void testUpdate() {
+		final OrderItems updated = new OrderItems(1L, 1L, 1L, 1L);
+		assertEquals(updated, DAO.update(updated));
+
+	}
+
+	@Test
+	public void testDelete() {
+		assertEquals(1, DAO.delete(1));
+	}
+
+}
