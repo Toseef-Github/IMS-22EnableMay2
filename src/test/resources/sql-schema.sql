a01a148181bf16a3aa5926ac1f0b5486746d04f6
Toseef-Github <105277464+Toseef-Github@users.noreply.github.com>
2022-06-09 22:58:18 +0100

    updated sql schema for testing

--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -1,4 +1,10 @@
+DROP TABLE IF EXISTS `order_items`;
+
+DROP TABLE IF EXISTS `orders`;
+
 DROP TABLE IF EXISTS `customers`;
+DROP TABLE IF EXISTS `items`;
+
 
 CREATE TABLE IF NOT EXISTS `customers` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
@@ -6,3 +12,28 @@ CREATE TABLE IF NOT EXISTS `customers` (
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
+
+CREATE TABLE IF NOT EXISTS `items` (
+    item_id INT(11) NOT NULL UNIQUE AUTO_INCREMENT,
+	item_name VARCHAR(40) DEFAULT NULL,
+    price DEC(11,2) DEFAULT NULL,
+    PRIMARY KEY (item_id)
+);
+
+CREATE TABLE IF NOT EXISTS `orders` (
+	order_id INT(11) NOT NULL AUTO_INCREMENT,
+    fk_id INT(11) NOT NULL,
+    PRIMARY KEY (order_id),
+    FOREIGN KEY (fk_id) REFERENCES customers(`id`)
+    
+);
+
+CREATE TABLE IF NOT EXISTS `order_items` (
+	order_item_id INT(11) NOT NULL UNIQUE AUTO_INCREMENT,
+    item_quantity INT DEFAULT 1,
+    fk_order_id INT(11) NOT NULL,
+    fk_item_id INT(11) NOT NULL,
+    PRIMARY KEY (order_item_id),
+    FOREIGN KEY (fk_order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (fk_item_id) REFERENCES items(item_id)
+    );
