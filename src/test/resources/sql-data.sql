0e35daf018d98aeda5a949e45e032833e3775928
Toseef-Github <105277464+Toseef-Github@users.noreply.github.com>
2022-06-09 22:57:44 +0100

    updated sql data for testing

--- src/test/resources/sql-data.sql
+++ src/test/resources/sql-data.sql
@@ -1,1 +1,4 @@
 INSERT INTO `customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
+INSERT INTO `items` (item_name, price) VALUES ('car', '5000');
+INSERT INTO `orders` (fk_id) VALUES ('1');
+INSERT INTO `order_items` (item_quantity, fk_order_id, fk_item_id) VALUES ('2', '1', '1');
