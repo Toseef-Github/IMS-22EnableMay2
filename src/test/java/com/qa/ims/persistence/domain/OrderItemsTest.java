edccdd15797252d5be5e15c882607ad3b690b4e3
Toseef-Github <105277464+Toseef-Github@users.noreply.github.com>
2022-06-09 19:24:05 +0100

    created order items domain test

---
+++ src/test/java/com/qa/ims/persistence/domain/OrderItemsTest.java
@@ -0,0 +1,13 @@
+package com.qa.ims.persistence.domain;
+
+import org.junit.Test;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+
+public class OrderItemsTest {
+
+	@Test
+	public void testEquals() {
+		EqualsVerifier.simple().forClass(OrderItems.class).verify();
+	}
+}
